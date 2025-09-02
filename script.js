009060e086002dec4f2680efaebfc9761c8d9929
idelfonso <idelfonso.neto1@aluno.ce.gov.br>
2025-09-02 16:28:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,7 @@ if(nome == "admin"){
 } else if(nome == "moderador" && idade>=16){
     alert("Olá, Moderador! Acesso liberado para a área de moderação.")
 }else if(idade>=18){
-    alert(`Olá 
-        ,${nome}! Você tem acesso completo. Boas-vindas à plataforma!`)
+    alert(`Olá ,${nome}! Você tem acesso completo. Boas-vindas à plataforma!`)
 } else if(idade >= 13 && idade <= 17 ) {
     alert("Acesso negado. Você pode participar do nosso programa Jovem Aprendiz!")
 } else {
